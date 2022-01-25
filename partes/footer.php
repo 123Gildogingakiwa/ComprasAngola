364d26dd2e2d6740a2d10d5ef4bcf05636795b5d
Gildo Ginga Kiwa <98285971+GildoGingaKiwa@users.noreply.github.com>
2022-01-25 19:08:13 +0000

    Add files via upload

---
+++ partes/footer.php
@@ -0,0 +1,47 @@
+<div class="footer">
+    <div class="container">
+        <div class="col-md-3 footer-grids fgd1">
+        <a href="index.html"><img src="images/logo2.png" alt=" " /><h3>Compras<span>$</span></h3></a>
+        <ul>
+            <li>Bairro Alvalade, Rua Paulo Adrade, Casa Nº 15</li>
+            <li>Luanda.</li>
+            <li><a href="mailto:zageshop@gmail.com">zageshop@gmail.com</a></li>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+        </ul>
+        </div>
+        <div class="col-md-3 footer-grids fgd2">
+            <h4>Informações</h4> 
+            <ul>
+                <li><a href="contact.php">Contactar-nos</a></li>
+                <li><a href="icons.php">Sobre nós</a></li>
+                <li><a href="typography.php">Typography</a></li>
+                <li><a href="faq.php">Perguntas Frequentes</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 footer-grids fgd3">
+            <h4>Produtos</h4> 
+            <ul>
+                <li><a href="jewellery.php">Joias</a></li>
+                <li><a href="cosmetics.php">Cosmeticos</a></li>
+                <li><a href="Shoes.php">Shoes</a></li>
+                <li><a href="deos.php">Perfumes</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 footer-grids fgd4">
+            <h4>Minha Conta</h4> 
+            <ul>
+                
+                <li><a href="login.php">Iniciar Sessão</a></li>
+                <li><a href="register.php">Registar-se</a></li>
+                <li><a href="recommended.php">Meu Carrinho</a></li>
+                <li><a href="payment.php">Pagamentos</a></li>
+                <li><a href="afiliadosarea.php">Aréa de Afiliados</a></li>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
+        <p class="copy-right">© 2016 Zageshop Todos direitos reservados |  Desenhado por <a href="index.php">Angocompras</a></p>
+    </div>
+</div>
---
+++ partes/menu.php
@@ -0,0 +1,134 @@
+<div class="header-top-w3layouts">
+	<div class="container">
+		<div class="col-md-6 logo-w3">
+			<a href="index.php"><img src="images/logo2.png" alt=" " /><h1>ComprasAngola</h1></a>
+		</div>
+		<div class="col-md-6 phone-w3l">
+			<ul>
+				<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span></li>
+				<li>+24499199199</li>
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<div class="header-bottom-w3ls">
+	<div class="container">
+		<div class="col-md-7 navigation-agileits">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header nav_2">
+					<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+						<span class="sr-only"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div> 
+				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+					<ul class="nav navbar-nav ">
+						<li class=" active"><a href="index.php" class="hyper "><span>Início</span></a></li>
+			
+	                  <li class="dropdown">
+							
+							<a href="#" class="dropdown-toggle  hyper" data-toggle="dropdown" ><span>Roupas<b class="caret"></b></span></a>
+								<ul class="dropdown-menu multi">
+									<div class="row">
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+			
+												<li><a href="women.php?id=R"><i class="fa fa-angle-right" aria-hidden="true"></i>Roupas de Mulher</a></li>
+												<li><a href="men.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Roupas de Homem</a></li>
+												<li><a href="kids.php"> <i class="fa fa-angle-right" aria-hidden="true"></i>Roupas de Crianças</a></li>
+												<li><a href="party.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Roupa de festa</a></li>
+										
+											</ul>
+										
+										</div>
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<li><a href="casuals.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Casuais</a></li>
+												<li><a href="night.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Roupa de noite</a></li>
+												<li><a href="formals.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Formais</a></li>
+												<li><a href="inner.php"><i class="fa fa-angle-right" aria-hidden="true"></i>íntima</a></li>
+										
+											</ul>						
+										</div>
+										<div class="col-sm-4 w3l">
+											<a href="women.php"><img src="images/menu1.jpg" class="img-responsive" alt=""></a>
+										</div>
+										<div class="clearfix"></div>
+									</div>	
+								</ul>
+						</li>
+					
+						<li class="dropdown">
+								<a href="#" class="dropdown-toggle hyper" data-toggle="dropdown" ><span> cuidados pessoais<b class="caret"></b></span></a>
+								<ul class="dropdown-menu multi multi1">
+									<div class="row">
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<li><a href="jewellery.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Jóias</a></li>
+												<li><a href="watches.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Relógios</a></li>
+												<li><a href="cosmetics.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Cosméticos</a></li>
+												<li><a href="deos.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Deo & Perfumes</a></li>
+										
+											</ul>
+											
+										</div>
+										<div class="col-sm-4">
+											
+											<ul class="multi-column-dropdown">
+												<li><a href="haircare.php"> <i class="fa fa-angle-right" aria-hidden="true"></i>Cuidado capilar </a></li>
+												<li><a href="shoes.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Sapatos</a></li>
+												<li><a href="handbags.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Bolsas</a></li>
+												<li><a href="skincare.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Cuidados com a pele</a></li>
+										
+											</ul>
+											
+										</div>
+										<div class="col-sm-4 w3l">
+											<a href="jewellery.php"><img src="images/menu2.jpg" class="img-responsive" alt=""></a>
+										</div>
+										<div class="clearfix"></div>
+									</div>	
+								</ul>
+						</li>
+
+
+						<li class="dropdown">
+								<a href="#" class="dropdown-toggle hyper" data-toggle="dropdown" ><span> Eletronicos <b class="caret"></b></span></a>
+								<ul class="dropdown-menu multi multi1">
+									<div class="row">
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<li><a href="newspro.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Novos</a></li>
+												<li><a href="segunda.php"><i class="fa fa-angle-right" aria-hidden="2mao"></i>Segunda Mão</a></li>
+												<li><a href="telefones.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Telefones</a></li>
+												<li><a href="computadores.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Computadores</a></li>
+										
+											</ul>
+											
+										</div>
+										<div class="col-sm-4">
+											
+											<ul class="multi-column-dropdown">
+												<li><a href="cozinha.php"> <i class="fa fa-angle-right" aria-hidden="true"></i>Para Cozinha</a></li>
+												<li><a href="sala.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Para Sala</a></li>
+												<li><a href="escritorio.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Escritórios</a></li>
+											
+											</ul>
+											
+										</div>
+										<div class="col-sm-4 w3l">
+											<a href="newspro.php"><img src="images/ele.jpg" class="img-responsive" alt=""></a>
+										</div>
+										<div class="clearfix"></div>
+									</div>	
+								</ul>
+						</li>
+						<li><a href="about.php" class="hyper"><span>Nossa Equipe</span></a></li>
+						<li><a href="contact.php" class="hyper"><span>Contactar</span></a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
